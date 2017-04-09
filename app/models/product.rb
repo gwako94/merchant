8a6260e758a578131df087580b63ca514d14c4f3
gwako94 <galgallowako254@gmail.com>
2017-04-09 16:29:05 +0300

    Designing orders

---
+++ app/models/product.rb
@@ -0,0 +1,9 @@
+class Product < ActiveRecord::Base
+	validates_numericality_of :price
+	def price=(input)
+    input.delete!("$")
+    super
+  end
+	validates :stock, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+	has_many :order_items
+end
